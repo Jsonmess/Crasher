9cc6f6e55eb001b48fd32536aced9833e6714942
jsonmess <shaopingz@jumei.com>
2017-02-17 17:02:15 +0800

    语言本地化

--- CrashAnalyseTool/MainWindowViewController.m
+++ CrashAnalyseTool/MainWindowViewController.m
@@ -9,8 +9,6 @@
 #import "MainWindowViewController.h"
 #import "DragDropView.h"
 
-#define DEFAULTTITLE  @"等待解析...\n\n小提示:\n  dsyml文件、crash日志文件和.app 确保放到同一个文件夹 ：）\n\n"
-
 @interface MainWindowViewController ()<DragDropViewDelegate>
 {
         NSString *_LogOut;//动态输出日志
@@ -40,14 +38,14 @@
     [self.logOutView setEditable:NO];
     [self.dragDropView registerForDraggedTypes:[NSArray arrayWithObjects:NSFilenamesPboardType, nil]];
     [self.dragDropView setDelegate:self];
-    [self.logOutView setString:DEFAULTTITLE];
+    [self.logOutView setString:NSLocalizedString(@"defaultTitle", nil)];
 }
 -(void)dragDropViewFileList:(NSArray *)fileList
 {
 //    NSLog(@"%@",fileList);
     self.crashFilePath = nil;
     self.dsymlFilePath = nil;
-    [self.logOutView setString:DEFAULTTITLE];
+    [self.logOutView setString:NSLocalizedString(@"defaultTitle", nil)];
     for (NSString* filePath in fileList) {
         if ([filePath rangeOfString:@".crash"].length > 0 )
         {
@@ -62,10 +60,10 @@
     
     if (self.crashFilePath == nil && self.crashFilePath.length <= 0)
     {
-        [self AlertViewShowWithMessageText:@"警告" InforMativeText:@"找不到crash日志文件"];
+        [self AlertViewShowWithMessageText:NSLocalizedString(@"warning", @"") InforMativeText:NSLocalizedString(@"nofile", @"")];
         return;
     }
-    _LogOut = @"解析中,请稍后...\n";
+    _LogOut = NSLocalizedString(@"analysing", @"");
     [self.logOutView setString:_LogOut];
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
      [self beginToAnalysisCrashFile];
@@ -82,7 +80,7 @@
     NSString *launchPath = @"/Applications/Xcode.app/Contents/SharedFrameworks/DVTFoundation.framework/Versions/A/Resources/symbolicatecrash";
     if (launchPath==nil||!([launchPath rangeOfString:@"Xcode.app"].length>0))
     {
-        [self AlertViewShowWithMessageText:@"警告" InforMativeText:@"找不到Xcode，请先前往AppStore 下载Xcode"];
+        [self AlertViewShowWithMessageText:NSLocalizedString(@"warning", @"")  InforMativeText:NSLocalizedString(@"noXcode", @"") ];
     }
     else
     {
@@ -102,7 +100,7 @@
     {
         dispatch_async(dispatch_get_main_queue(), ^{
             NSString *logoutFilePath = [[self.crashFilePath stringByDeletingPathExtension] stringByAppendingPathExtension:@"log"];
-            _LogOut = [_LogOut stringByAppendingString:[NSString stringWithFormat:@"解析完成..\n正在写入文件\n%@\n",logoutFilePath]];
+            _LogOut = [_LogOut stringByAppendingString:[NSString stringWithFormat:@"%@%@\n",NSLocalizedString(@"finishedAnalyseLog", @""),logoutFilePath]];
             [self.logOutView setString:_LogOut];
         });
     }
@@ -123,13 +121,13 @@
     if (error)
     {
         dispatch_async(dispatch_get_main_queue(), ^{
-            [self AlertViewShowWithMessageText:@"提示" InforMativeText:@"解析内容写入文件失败，请重试"];
+            [self AlertViewShowWithMessageText:NSLocalizedString(@"alert", @"") InforMativeText:NSLocalizedString(@"errorWriteLog", @"")];
         });
     }
     else
     {
         dispatch_async(dispatch_get_main_queue(), ^{
-            _LogOut = [_LogOut stringByAppendingString:@"写入文件成功！"];
+            _LogOut = [_LogOut stringByAppendingString:NSLocalizedString(@"successWriteLog",@"")];
             [self.logOutView setString:_LogOut];
         });
 
@@ -138,7 +136,7 @@
 -(void)AlertViewShowWithMessageText:(NSString *)message InforMativeText:(NSString*)info
 {
     
-    NSAlert *alert=[NSAlert alertWithMessageText:message defaultButton:@"确认" alternateButton:@"取消" otherButton:nil informativeTextWithFormat: info,nil];
+    NSAlert *alert=[NSAlert alertWithMessageText:message defaultButton:NSLocalizedString(@"confirm",@"") alternateButton:NSLocalizedString(@"cancel",@"") otherButton:nil informativeTextWithFormat: info,nil];
     
     [alert beginSheetModalForWindow:[NSApplication sharedApplication].keyWindow completionHandler:nil];
     
